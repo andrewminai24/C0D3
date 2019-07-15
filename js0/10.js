15ed4e4f77cc342f2e30d746516d85e4c60fa21a
andrewminai24 <andrewminai24@gmail.com>
2019-07-14 23:54:14 -0700

    added 10.js

---
+++ js0/10.js
@@ -0,0 +1,18 @@
+/**
+ * Write a function called solution that
+ * Takes in 2 functions, returns
+ *   the sum of the result of calling the 2 functions
+ * @param {function} a
+ * @param {function} b
+ * @param {number} c
+ */
+
+const solution = (a, b) => {
+  return 0
+}
+  return a() + b();
+
+}
+module.exports = {
+  solution
+}
