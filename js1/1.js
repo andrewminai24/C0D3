fa93149bc2a0f49dac6782117ae1ec8c7a9989e1
andrewminai24 <andrewminai24@gmail.com>
2019-07-15 00:04:42 -0700

    js1 1.js

---
+++ js1/1.js
@@ -0,0 +1,18 @@
+/**
+ * Write a function called solution that
+ *   takes in a number and returns a function.
+ * When the returned function is called, the next incremental number
+ *   is returned
+ * @param {number} a
+ * @returns {function}
+ */
+
+const solution = a => {
+  return () => {
+    return ++a;
+  };
+};
+
+module.exports = {
+  solution
+};
